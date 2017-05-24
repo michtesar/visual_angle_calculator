12f02409683cfb4abcaf14da5436240a54514efc
Michael Tesař <michael.tesar@nudz.cz>
2017-05-24 08:06:05 +0200

    Working logics

---
+++ script.js
@@ -0,0 +1,32 @@
+function convertToAngle() {
+    var h = document.getElementById('angle-h').value;
+    var d = document.getElementById('angle-d').value;
+    var res = document.getElementById('angle-r').value;
+    var size = document.getElementById('angle-s').value;
+    
+    if (h && d && res && size) {
+        degPerPix = ((Math.atan2(0.5*h, d)) / (0.5 * res)) * 180 / Math.PI;
+        sizeInDeg = size * degPerPix;
+        alert('Single pixel is equal to: ' + Number(degPerPix).toFixed(4) + '\n' 
+        + 'Stimulus size in visual angle is equal to: ' + Number(sizeInDeg).toFixed(4));
+    } else {
+        alert('Enter all parameters in a box with numbers.');
+    }
+    
+}
+
+function convertToPixel() {
+    var h = document.getElementById('pixel-h').value;
+    var d = document.getElementById('pixel-d').value;
+    var res = document.getElementById('pixel-r').value;
+    var size = document.getElementById('pixel-s').value;
+
+    if (h && d && res && size) {
+        degPerPix = ((Math.atan2(0.5*h, d)) / (0.5 * res)) * 180 / Math.PI;
+        sizeInDeg = size / degPerPix;
+        alert('Single pixel is equal to: ' + Number(degPerPix).toFixed(4) + '\n' 
+        + 'Stimulus size in pixels is equal to: ' + Number(sizeInDeg).toFixed(4));
+    } else {
+        alert('Enter all parameters in a box with numbers.');
+    }
+}
